3b4ef8f0b2d6bf743f0c61833e2f3a358fde90f5
pkg415 <115843385+pkg415@users.noreply.github.com>
2023-10-15 15:37:25 -0700

    set up App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import "./style.css";
 
 function App() {
-  return <h1>Are you in there Chief Inspector?!?</h1>
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="113" width="113" alt="butterflies logo" />
+        <h1>Hello Chief Inspector</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a tidbit</button>
+    </header>
+  );
 }
 
 export default App;
